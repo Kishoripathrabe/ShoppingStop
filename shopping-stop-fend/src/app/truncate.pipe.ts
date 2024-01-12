6356134539b1abf9ccfa1f4f2c544b997e6e74e7
kishori-pathrabe <kishoripathrabe123@gmail.com>
2024-01-12 21:45:33 +0530

    Fixed Pipe For less than value passed

--- shopping-stop-fend/src/app/truncate.pipe.ts
+++ shopping-stop-fend/src/app/truncate.pipe.ts
@@ -6,7 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TruncatePipe implements PipeTransform {
 
   transform(value: any, ...args: any[]) {
-    return value.slice(0,args[0])+"...";
+    if(value.length>args[0]){
+      return value.slice(0,args[0])+"...";
+    } else {
+      return value;
+    }
+
   }
 
 }

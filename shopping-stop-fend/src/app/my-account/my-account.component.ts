cc72bf1e7f633626f7d95bc9bd6cbf12400eef2b
kishori-pathrabe <kishoripathrabe123@gmail.com>
2024-01-16 02:00:16 +0530

    CREATED: My Account functionality

--- shopping-stop-fend/src/app/my-account/my-account.component.ts
+++ shopping-stop-fend/src/app/my-account/my-account.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-my-account',
   templateUrl: './my-account.component.html',
   styleUrls: ['./my-account.component.scss']
 })
-export class MyAccountComponent {
+export class MyAccountComponent implements OnInit {
+  user: any = {}; 
+  isEditMode = false; 
+  constructor(private userService: UserService) { }
 
+  ngOnInit(): void {
+   this.setUser();
+  }
+  toggleEditMode(): void {
+    this.isEditMode = !this.isEditMode;
+  }
+  saveChanges(): void {
+    this.userService.updateMe(this.user).subscribe(data=>{
+      this.isEditMode = false; // Disable edit mode after saving changes
+      console.log(this.user,data);
+      this.setUser();
+    })
+  }
+  setUser() {
+    this.userService.fetchMe().subscribe(
+      (data: any) => {
+        this.user = {email:data?.email,first_name:data.first_name,last_name:data.last_name,password:data.password};
+      },
+      (error) => {
+        console.error('Error fetching user data:', error);
+      }
+    );
+  }
 }
---
+++ shopping-stop-fend/src/app/services/user.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthUtils } from '../utility/auth-utils';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = 'http://localhost:5000/api/admin'; // Replace with your actual API endpoint
+  private apiUserUrl = 'http://localhost:5000/api/user';
+  constructor(private http: HttpClient) {}
+
+
+  getAuthHeaders() {
+    return {authorization: `bearer ${AuthUtils.getAuthToken()}`};
+  }
+
+  fetchMe() {
+    return this.http.get(this.apiUserUrl + "/get/fetchme",{headers : this.getAuthHeaders() })
+  }
+  updateMe(data: any){
+    return this.http.patch(this.apiUserUrl + "/update/me",{updatedMe:data},{headers : this.getAuthHeaders() })
+
+  }
+}

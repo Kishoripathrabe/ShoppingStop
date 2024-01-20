d3936de8676549ec00e533fead5202faff546909
kishori-pathrabe <kishoripathrabe123@gmail.com>
2024-01-20 21:56:57 +0530

    Created: ordered page and navigate to home

--- shopping-stop-fend/src/app/ordered/ordered.component.ts
+++ shopping-stop-fend/src/app/ordered/ordered.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ordered',
   templateUrl: './ordered.component.html',
   styleUrls: ['./ordered.component.scss']
 })
-export class OrderedComponent {
-
+export class OrderedComponent implements OnInit{
+  constructor(private router: Router){}
+  ngOnInit() {
+    setTimeout(() => {
+      this.router.navigate(['customerpage','home']); 
+    }, 4000); 
+  }
+  
 }
